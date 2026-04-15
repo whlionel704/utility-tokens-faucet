2a49a8c2dcdd62d78069076a559c15e930577930
whlionel704 <whlionel704@gmail.com>
2026-04-15 23:40:35 +0800

    unit test fixture

--- test/UtilityToken.test.ts
+++ test/UtilityToken.test.ts
@@ -1,19 +1,27 @@
 import { describe, it } from "node:test";
-import assert from "node:assert";
 import hre from "hardhat";
 import { keccak256, toBytes, getAddress } from "viem";
 
 const { viem } = await hre.network.connect();
 
 describe("UtilityToken", function () {
-  it.only("Should allow minter to mint tokens successfully", async () => {
-    const [admin, minter] = await viem.getWalletClients();
-
+  async function getWalletClients() {
+    const [admin, minter, nonMinter] = await viem.getWalletClients();
+    return { admin, minter, nonMinter };
+  }
+  async function deployFixtures() {
+    const { admin } = await getWalletClients();
     const utilityToken = await viem.deployContract("UtilityToken", [
       "UtilityToken",
       "UTK",
       admin.account.address,
     ]);
+    return { utilityToken };
+  }
+
+  it("Should allow minter to mint tokens successfully", async () => {
+    const { admin, minter } = await getWalletClients();
+    const { utilityToken }= await deployFixtures();
 
     await utilityToken.write.grantRole([keccak256(toBytes("MINTER_ROLE")), minter.account.address], { account: admin.account });
 
@@ -32,12 +40,8 @@ describe("UtilityToken", function () {
   });
 
   it("Should throw access control error if attempting to call mintTokens using an address which is not the minter role", async () => {
-    const [admin, nonMinter] = await viem.getWalletClients();
-    const utilityToken = await viem.deployContract("UtilityToken", [
-      "UtilityToken",
-      "UTK",
-      admin.account.address,
-    ]);
+    const { admin, nonMinter } = await getWalletClients();
+    const { utilityToken }= await deployFixtures();
     await viem.assertions.revertWithCustomError(
       utilityToken.write.mintTokens([nonMinter.account.address, 1000n], { account: admin.account }),
       utilityToken,
